914e070a757768e0c2ddaccebf2f057601c827e2
weou384l <lineon083@gmail.com>
2025-05-11 21:57:44 +0330

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,7 +14,7 @@ async function getUserNetworkInfo() {
 function renderLogin() {
   app.innerHTML = `
     <div class="flex items-center justify-center min-h-screen px-4">
-      <div class="bg-gray-900 p-8 rounded-2xl shadow-2xl max-w-sm w-full">
+      <div class="login-box p-8 rounded-2xl shadow-2xl max-w-sm w-full">
         <h2 class="text-2xl font-bold text-center mb-6">ورود به حساب</h2>
         <input id="username" type="text" placeholder="نام کاربری"
           class="w-full p-3 mb-4 rounded bg-gray-800 text-white focus:outline-none" />
@@ -25,6 +25,7 @@ function renderLogin() {
     </div>
   `;
 }
+
 
 function renderDashboard(networkInfo) {
 document.body.className = 'dashboard-background';
