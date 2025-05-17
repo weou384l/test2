e3954fbdad094642ee11a37343cdc01ffc1893b2
weou384l <lineon083@gmail.com>
2025-05-18 02:41:02 +0330

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,7 +24,7 @@ function checkHttps() {
 }
 
 function getIpInfo() {
-  fetch("https://ipinfo.io/json?token=YOUR_TOKEN_HERE")
+  fetch("https://ipinfo.io/json?token=8824fa830e1d01")
     .then(response => response.json())
     .then(data => {
       const info = `IP: ${data.ip} - ${data.city}, ${data.country}`;
